e33bbdd8f0716ca3e1687cbcbdbfda0c03abcc26
prabhu <prabhu.mc2@gmail.com>
2026-05-25 11:07:57 -0400

    Formating files for ruff format check using ruff check . --fix

--- src/ai_learning/nlp/removing_stropwords.py
+++ src/ai_learning/nlp/removing_stropwords.py
@@ -15,26 +15,33 @@ Steps:
 
 import nltk
 from nltk.corpus import stopwords
-nltk.download('stopwords')
+
+nltk.download("stopwords")
 
 # Original string
 inp_str = "It was too far to go to the shop and he did not want her to walk"
 
 # store the stop words in a variable
-en_stp = stopwords.words('English')
-tam_stp = stopwords.words('Tamil')
+en_stp = stopwords.words("English")
+tam_stp = stopwords.words("Tamil")
 
 
 # filter all the stop words and create a list ( list comprehension). Don't forget to create lower case
-filtered_txt = " ".join([item for item in inp_str.lower().split() if item not in en_stp])
+filtered_txt = " ".join(
+    [item for item in inp_str.lower().split() if item not in en_stp]
+)
 print(f"Filtered text : {filtered_txt: >10}")
 
 # You can add words to stop string because it's a list
-en_stp.append('go')
-filtered_txt = " ".join([item for item in inp_str.lower().split() if item not in en_stp])
+en_stp.append("go")
+filtered_txt = " ".join(
+    [item for item in inp_str.lower().split() if item not in en_stp]
+)
 print(f"Filtered text : {filtered_txt: >10}")
 
 # You can also add words to the stop word dictionary
-en_stp.remove('the')
-filtered_txt = " ".join([item for item in inp_str.lower().split() if item not in en_stp])
+en_stp.remove("the")
+filtered_txt = " ".join(
+    [item for item in inp_str.lower().split() if item not in en_stp]
+)
 print(f"Filtered text : {filtered_txt: >10}")
--- tests/test_nlp.py
+++ tests/test_nlp.py
@@ -1,8 +1,9 @@
-'''
+"""
 Test for NLP modules
 
 This test module tests the nlp features implemented
-'''
+"""
+
 
 def test_hello_nlp():
     assert True
