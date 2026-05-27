5a79eb9699208e63b09576b848504e42a7e46312
prabhu <prabhu.mc2@gmail.com>
2026-05-27 12:11:20 -0400

    Adding tests to check nlp and removing unwanted code and renaming the test method

--- src/ai_learning/nlp/removing_stropwords.py
+++ src/ai_learning/nlp/removing_stropwords.py
@@ -19,39 +19,54 @@ from nltk.corpus import stopwords
 nltk.download("stopwords")
 
 
-def filter_stop_words(inp_str: str, lang: str = "eng") -> str:
-    """
-    This method will filter the stop words from the string
-    :param inp_str: Input string where stop words will be removed from
-    :param lang: language
-    :return: String with removed stop words
-    """
-    if lang == "tamil":
-        stp_list = stopwords.words("tamil")
-    else:
-        stp_list = stopwords.words("english")
+class FilterStopWords:
+    stp_list = None
 
-    status = " ".join(
-        [word for word in inp_str.lower().split() if word not in stp_list]
-    )
-    return status
+    @classmethod
+    def load_stop_words(cls, lang: str = "english") -> None:
+        """
+        Loads the stop words to stp_list
+        :param lang: Language
+        """
+        if lang.lower() == "tamil":
+            cls.stp_list = stopwords.words("tamil")
+        else:
+            cls.stp_list = stopwords.words("english")
 
+    @classmethod
+    def get_stopwords(cls) -> list:
+        """This method will return the stop words list"""
+        return cls.stp_list
 
-def append_stopwords(word: str, stp_list: list) -> None:
-    """
-    Method to add new words to stop word list.
-    :param word: Word to be added to stop word list
-    :param stp_list: Stop word list
-    :return: None
-    """
-    stp_list.append(word)
+    @classmethod
+    def filter_stop_words(cls, inp_str: str, lang: str = "english") -> str:
+        """
+        This method will filter the stop words from the string
+        :param inp_str: Input string where stop words will be removed from
+        :param lang: language
+        :return: String with removed stop words
+        """
+        if cls.stp_list is None:
+            cls.load_stop_words(lang)
+        status = " ".join(
+            [word for word in inp_str.lower().split() if word not in cls.stp_list]
+        )
+        return status
 
+    @classmethod
+    def append_stopwords(cls, word: str) -> None:
+        """
+        Method to add new words to stop word list.
+        :param word: Word to be added to stop word list
+        :return: None
+        """
+        cls.stp_list.append(word)
 
-def remove_stopwords(word: str, stp_list: dict) -> None:
-    """
-    Method to remove a word from stop word list
-    :param word: Word to be removed
-    :param Stop word list : stop word list
-    :return:
-    """
-    stp_list.remove(word)
+    @classmethod
+    def remove_stopwords(cls, word: str) -> None:
+        """
+        Method to remove a word from stop word list
+        :param word: Word to be removed
+        :return:
+        """
+        cls.stp_list.remove(word)
